baf8dde068310a1017965a388d16b75aa554d4e3
Fernando <FernandoNieto71>
2021-04-23 19:17:39 -0300

    mod  ejercicio 4

--- app/ejercicio4.php
+++ app/ejercicio4.php
@@ -8,18 +8,18 @@
 	
 	if($numero1 > $numero2 && $numero1 > $numero3)
 		{
-			echo "El numero ", $numero1 ," es el mayor de ";
+			echo "El numero ", $numero1 ," es el mayor";
 		}
 	else
 		{
 		if($numero2 > $numero3)
 			{
-				echo "El numero ", $numero2, " es el mayor de ";
+				echo "El numero ", $numero2, " es el mayor";
 			}
 		else
 			{
 				
-				echo "El numero ", $numero3, " es el mayor de ";
+				echo "El numero ", $numero3, " es el mayor";
 				
 					
 
