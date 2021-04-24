de963208bae8777248b7f5b8b044b0d82db08a67
Fernando <FernandoNieto71>
2021-04-23 21:18:09 -0300

    ejerc 7

--- app/ejercicio7.php
+++ app/ejercicio7.php
@@ -1,14 +1,16 @@
 <?php
 	echo "<h1>(IF) de tres numeros indicar el numero del medio o informar si No hay numero del medio  </h1>";
 	
-	/*$numero1 = rand(1, 49);
-	$numero2 = rand(1, 25);
-	$numero3 = rand(1, 75);*/
-	$numero1 = 7;
-	$numero2 = 5;
-	$numero3 = 3;
+
+	$numero1 = 3;
+	$numero2 = 7;
+	$numero3 = 5;
 
 	
+if($numero1 == $numero2 || $numero1 == $numero3 || $numero2 == $numero3){
+						echo "No tiene numero del medio";
+}
+else{
 	if(($numero1 > $numero2 && $numero1 < $numero3) || ($numero1 < $numero2 && $numero1 > $numero3))
 		{
 			echo "El numero ", $numero1 ," es el del medio";
@@ -21,13 +23,10 @@
 			}
 		else
 			{
-				if(($numero3 > $numero1 && $numero3 < $numero2) || ($numero3 < $numero1 && $numero3 > $numero2)){
-						echo "El numero ", $numero3, " es el del medio";
-					}
-				else{
-						echo "No tiene numero del medio";
-					}
+				echo "El numero ", $numero3, " es el del medio";
 			}
 		}
+}
+	
 
 ?>
