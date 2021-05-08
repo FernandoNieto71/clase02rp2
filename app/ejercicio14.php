eabea1bb804f8e416476dfe30b46a4765d24d0d9
Fernando <FernandoNieto71>
2021-05-07 21:37:12 -0300

    ejercicio14

---
+++ app/ejercicio14.php
@@ -0,0 +1,43 @@
+<?php
+/*primer funcion de f° a C° (1 param) retorma valor
+segunda C a F  C° a f° (1 param) retorma valor
+tercera mostrartemperatura (param que sea necesaria)
+cuarta transformar (una temp, a que la quiere llevar)
+*/
+//include_once "calcular.php";
+$temperatura = rand(-20, 125);
+//$operacion = "celsius";
+$operacion	= "fahrenheit";
+
+mostrartemperatura($temperatura, $operacion);
+
+function fahrenheitCelsius($temperatura){
+	return ($temperatura-32)*5/9;
+}
+
+function celsiusFahrenheit($temperatura){
+	return ($temperatura * 9/5) + 32;
+}
+
+function mostrarTemperatura($valortermico, $operacion){
+	$valor = transformarA($valortermico, $operacion);
+	$otraop;
+	if($operacion == "celsius"){
+		$otraop = "fahrenheit";
+	}
+	else{
+		$otraop = "celsius";
+	}
+	echo "La temperatura $valortermico en $otraop es igual a $valor en $operacion";
+}
+
+function transformarA($valortermico, $operacion){
+	if($operacion=="celsius"){
+			return fahrenheitCelsius($valortermico);
+		} else{
+				return celsiusFahrenheit($valortermico);
+		}
+		
+}
+
+?>
